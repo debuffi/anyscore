52e57625d2969820e01bee46119c5f7df2d6bfb0
debuffi <debuffi@yahoo.com>
2022-09-10 16:07:21 +0300

    step

--- src/main/java/tv/cybergames/controller/TestController.java
+++ src/main/java/tv/cybergames/controller/TestController.java
@@ -1,15 +1,19 @@
 package tv.cybergames.controller;
 
+import java.io.File;
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.minio.GetObjectResponse;
 import io.minio.messages.Bucket;
 import lombok.RequiredArgsConstructor;
 import tv.cybergames.domain.dto.PlayerDto;
 import tv.cybergames.domain.dto.StatsPlayerDto;
+import tv.cybergames.domain.dto.file.FileUploadResponse;
 import tv.cybergames.service.GameInfo;
 import tv.cybergames.service.MinioService;
 
@@ -31,7 +35,7 @@ public class TestController {
     }
 
 
-    @GetMapping("/ststs/players")
+    @GetMapping("/stats/players")
     public List<StatsPlayerDto> getStatsPlayers() {
         return hltvBasedGameInfo.getStatsPlayers();
     }
@@ -44,5 +48,25 @@ public class TestController {
     @GetMapping("/minio/buckets")
     public List<String> getAllBuckets() {
         return minioService.getAllBuckets().stream().map(Bucket::name).toList();
+    }
+
+    @GetMapping("/minio/delete")
+    public void removeObject() {
+        minioService.deleteFile("test", "123.png");
+    }
+
+    @GetMapping("/minio/create")
+    public FileUploadResponse createOPbject() {
+        return minioService.uploadFile("test", new File("123.png"));
+    }
+
+    @GetMapping("/minio/file")
+    public void getFileByPath() {
+         minioService.getFileByPath("test", "123.png");
+    }
+
+    @GetMapping("/minio/exists")
+    public boolean objectExists() {
+        return minioService.fileExists("test", "123.png");
     }
 }
